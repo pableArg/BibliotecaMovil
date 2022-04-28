e4c2f11d5b209d181eefee0fd2f216356fa47968
pableArg <pablo.jc95@hotmail.com>
2022-04-28 14:21:09 -0300

    Second Commit

---
+++ app/src/main/java/com/example/bibliotecamovil/Activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bibliotecamovil.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bibliotecamovil.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
